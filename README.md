1d8c335ca94145de972b9d3e210253e543971c6b
billumandal <billu.mandal@gmail.com>
2015-12-22 18:13:11 +0530

    First view done. Now thinking about adding database, need to see the algorithm

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # billuisin
 This is the repo for my site from which i'll link to quiz and my other products in Heroku &amp; Amazon &amp; Google
+
+This is inside a Virtual environment, so set it up prior to cloning this.
+
+Will use Node to do something with React.js
---
+++ billuisin/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+
+def home(request):
+	# template_name = 'index.html'
+	return render(request, 'index.html')
