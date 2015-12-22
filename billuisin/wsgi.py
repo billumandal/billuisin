fc159c12c5a9201ab99e7de08f5b614a8e1d192c
billumandal <billu.mandal@gmail.com>
2015-12-22 10:05:56 +0530

    Initial commit

---
+++ billuisin/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for billuisin project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "billuisin.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
