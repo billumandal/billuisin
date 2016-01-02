6a8c8a5fb2ebf2fb478f71d0d2a4e2504687893e
billumandal <billu.mandal@gmail.com>
2016-01-02 09:03:33 +0530

    I dunno what I added

--- quiz/forms.py
+++ quiz/forms.py
@@ -4,21 +4,36 @@ from django.shortcuts import render
 from models import QuizAttempt, Question, Answer
 
 class AnswerForm(forms.ModelForm):
-    class Meta:
-        model = Answer
-        correct_answer = models.MultipleChoiceField(widget=form.RadioSelect)
-        fields = ('id','Question.question_text', 'correct_answer',)
+	def get_option_w(self, obj):
+		return obj.id.option_w
+	def get_option_x(self, obj):
+		return obj.id.option_x
+	def get_option_y(self, obj):
+		return obj.id.option_y
+	def get_option_z(self, obj):
+		return obj.id.option_z
+	ANSWER_CHOICES = (
+		('get.option_w', 'get.option_w'),
+		('get.option_x', 'get.option_x'),
+		('get.option_y', 'get.option_y'),
+		('get.option_z', 'get.option_z'),
+	)
 
-class QuizAttempt(forms.Form):
-	subject = forms.ManyToManyField(Subject)
-	question_text = forms.TextField(max_length=150)
-	option_w = forms.CharField(max_length=75, null=True)
-	option_x = forms.CharField(max_length=75, null=True)
-	option_y = forms.CharField(max_length=75, blank=True)
-	option_z = forms.CharField(max_length=75, blank=True)
-	correct_answer = forms.ChoiceField(choices=(('(a)', option_w),
-												('(b)', option_x),
-												('(c)', option_y),
-												('(d)', option_z),), 
-										widget=forms.RadioSelect(), 
-										verbose_name='Select your answer')
+	class Meta:
+		model = Answer
+		correct_answer = forms.MultipleChoiceField(widget=forms.RadioSelect, choices='ANSWER_CHOICES')
+		fields = ('id', 'correct_answer',)
+
+# class QuizAttempt(forms.Form):
+# 	subject = forms.CharField(max_length=17)
+# 	question_text = forms.TextField(max_length=150)
+# 	option_w = forms.CharField(max_length=75, null=True)
+# 	option_x = forms.CharField(max_length=75, null=True)
+# 	option_y = forms.CharField(max_length=75, blank=True)
+# 	option_z = forms.CharField(max_length=75, blank=True)
+# 	correct_answer = forms.ChoiceField(choices=(('(a)', option_w),
+# 												('(b)', option_x),
+# 												('(c)', option_y),
+# 												('(d)', option_z),), 
+# 										widget=forms.RadioSelect(), 
+# 										verbose_name='Select your answer')
